39e2d693086edd37afd51c072437f99e16ade4d1
Jacob Ismael <jacob.ismael@gmail.com>
2026-03-06 11:23:17 -0800

    added reg writing

---
+++ src/address.py
@@ -0,0 +1,15 @@
+joint_pos = [0, 0, 0, 0, 0, 0, 0]
+joint_addresses = (776, 778, 780, 782, 784, 786, 788)
+
+# X, Y, Z
+tool_pos = [0, 0, 0]
+tool_addresses = (790, 792, 794)
+
+distance_units = [0]
+distance_units_address = (771)
+
+command_value_registers = [0, 0, 0, 0, 0, 0, 0]
+command_value_addresses = (1024, 1026, 1028, 1030, 1032, 1034, 1036)
+
+command_regiser = [0]
+command_address = (1038)
--- src/robot.py
+++ src/robot.py
@@ -1,18 +1,10 @@
 from pyModbusTCP.client import ModbusClient
 import utils
+from address import *
 
 SERVER_HOST = "192.168.0.100"
 SERVER_PORT = 5020
 c = ModbusClient(host=SERVER_HOST, port=SERVER_PORT, auto_open=True, auto_close=True)
-
-JOINT_OFFSET = 776
-NUM_JOINTS = 7
-
-joint_addresses = (776, 778, 780, 782, 784, 786, 788)
-joint_pos = [0, 0, 0, 0, 0, 0, 0]
-
-tool_addresses = (790, 792, 794)
-tool_pos = [0, 0, 0]
 
 def print_positions(positions: list) -> None:
     output_str = []
@@ -22,7 +14,7 @@ def print_positions(positions: list) -> None:
         index = index+1
     print(output_str)
 
-def get_address_positions(addresses: list, positions: list) -> None:
+def get_address_positions(addresses: list[int], positions: list) -> None:
     index = 0
     for address in addresses:
         reg_list = c.read_input_registers(address, 2)
@@ -33,3 +25,23 @@ def get_address_positions(addresses: list, positions: list) -> None:
             print(f'Error: could not read {index+1} position')
             positions[index] = -1
         index = index + 1
+
+def write_addresses(addresses: list[int], values: list[float]) -> bool:
+    for address, value in zip(addresses, values):
+        utils.float_to_registers()
+        if not c.write_multiple_registers(address, value):
+            print(f'Error: could not write {address} register')
+            return False
+        return True
+
+def move_joint_position(joints: list) -> None:
+    if not write_addresses(command_value_addresses, joints):
+        print("Error: Couldn't write to command argument registers. Aborting command")
+    write_addresses(command_address, [1])
+
+def move_tool_position(x , y, z, roll = 0.0, pitch = 0.0, yaw = 0.0) -> None:
+    # implement limiters
+    args = [x, y, z, roll, pitch, yaw]
+    if not write_addresses(command_value_addresses, args):
+        print("Error: Couldn't write to command argument registers. Aborting command")
+    write_addresses(command_address, [2])
