8ba5edefb30cdbc6691bdf1405bebee5e4e4b4d0
Jacob Ismael <jacob.ismael@gmail.com>
2026-03-06 10:35:52 -0800

    added tool pos

--- src/main.py
+++ src/main.py
@@ -3,14 +3,17 @@ import time
 import logging
 import robot
 import sys
+import os
 
 if('-debug' in sys.argv or '-d' in sys.argv):
     logging.basicConfig()
     logging.getLogger('pyModbusTCP.client').setLevel(logging.DEBUG)
 
 if __name__ == '__main__':
-    robot.get_joint_positions()
     while True:
-        robot.get_joint_positions()
-        robot.print_joint_positions()
+        robot.get_address_positions(robot.tool_addresses, robot.tool_pos)
+        robot.print_positions(robot.tool_pos)
+        robot.get_address_positions(robot.joint_addresses, robot.joint_pos)
+        robot.print_positions(robot.joint_pos)
+        os.system('clear')
         time.sleep(1)
--- src/robot.py
+++ src/robot.py
@@ -1,6 +1,5 @@
 from pyModbusTCP.client import ModbusClient
 import utils
-import os
 
 SERVER_HOST = "192.168.0.100"
 SERVER_PORT = 5020
@@ -12,23 +11,25 @@ NUM_JOINTS = 7
 joint_addresses = (776, 778, 780, 782, 784, 786, 788)
 joint_pos = [0, 0, 0, 0, 0, 0, 0]
 
-def print_joint_positions() -> None:
+tool_addresses = (790, 792, 794)
+tool_pos = [0, 0, 0]
+
+def print_positions(positions: list) -> None:
     output_str = []
-    joint = 1
-    for pos in joint_pos:
-        output_str.append(f'J{joint}: {pos:.2f}')
-        joint = joint+1
-    os.system('clear')
+    index = 1
+    for pos in positions:
+        output_str.append(f'{index}: {pos:.2f}')
+        index = index+1
     print(output_str)
 
-def get_joint_positions() -> tuple:
-    joint = 0
-    for address in joint_addresses:
+def get_address_positions(addresses: list, positions: list) -> None:
+    index = 0
+    for address in addresses:
         reg_list = c.read_input_registers(address, 2)
         if reg_list is not None:
             (reg_low, reg_high) = reg_list
-            joint_pos[joint] = utils.registers_to_float(reg_low, reg_high)
+            positions[index] = utils.registers_to_float(reg_low, reg_high)
         else:
-            print(f'Error: could not read joint {joint+1} position')
-            joint_pos[joint] = -1
-        joint = joint + 1
+            print(f'Error: could not read {index+1} position')
+            positions[index] = -1
+        index = index + 1
