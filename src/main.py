6668b4a8304c2b74e0fa990a4315f276b4cbd092
Jacob Ismael <jacob.ismael@gmail.com>
2026-03-06 10:24:27 -0800

    added debug info

--- src/main.py
+++ src/main.py
@@ -2,12 +2,15 @@ from pyModbusTCP.client import ModbusClient
 import time
 import logging
 import robot
+import sys
 
-logging.basicConfig()
-logging.getLogger('pyModbusTCP.client').setLevel(logging.DEBUG)
+if('-debug' in sys.argv or '-d' in sys.argv):
+    logging.basicConfig()
+    logging.getLogger('pyModbusTCP.client').setLevel(logging.DEBUG)
 
 if __name__ == '__main__':
     robot.get_joint_positions()
     while True:
-        print(robot.joint_pos)
+        robot.get_joint_positions()
+        robot.print_joint_positions()
         time.sleep(1)
--- src/robot.py
+++ src/robot.py
@@ -1,8 +1,9 @@
 from pyModbusTCP.client import ModbusClient
 import utils
+import os
 
-SERVER_HOST = "127.0.0.1"
-SERVER_PORT = 502
+SERVER_HOST = "192.168.0.100"
+SERVER_PORT = 5020
 c = ModbusClient(host=SERVER_HOST, port=SERVER_PORT, auto_open=True, auto_close=True)
 
 JOINT_OFFSET = 776
@@ -10,6 +11,15 @@ NUM_JOINTS = 7
 
 joint_addresses = (776, 778, 780, 782, 784, 786, 788)
 joint_pos = [0, 0, 0, 0, 0, 0, 0]
+
+def print_joint_positions() -> None:
+    output_str = []
+    joint = 1
+    for pos in joint_pos:
+        output_str.append(f'J{joint}: {pos:.2f}')
+        joint = joint+1
+    os.system('clear')
+    print(output_str)
 
 def get_joint_positions() -> tuple:
     joint = 0
